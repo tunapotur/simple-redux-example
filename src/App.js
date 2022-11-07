7879a7a15d7e576fbb88ee2c838dd6d2fdb05cfc
Ahmet Tuna POTUR <30650371+tunapotur@users.noreply.github.com>
2022-11-07 14:07:09 +0300

    incremend, decrement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
 import Counter from "./components/Counter";
+
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 import "./App.css";
 
 function App() {
-  return <Counter />;
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
 }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,23 @@
+import React from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../redux/counter/counterSlice";
+
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const countValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className={classes.counter}>
       <div className={classes.counter__wrapper}>
-        <h1>1</h1>
-
+        <h1>{countValue}</h1>
         <div className={classes.counter__operations}>
           <div className={classes.counter__buttons}>
-            <button>Decrement</button>
-            <button>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(increment())}>Increment</button>
           </div>
-
           <div className={classes.counter__amount}>
             <input type="number" />
             <button>Increment by Amount</button>
