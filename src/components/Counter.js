23860dd185615c1f6c8c8768b646a1b8bf809079
Ahmet Tuna POTUR <30650371+tunapotur@users.noreply.github.com>
2022-11-07 13:31:44 +0300

    css

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,16 +3,20 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   return (
     <div className={classes.counter}>
-      <h1>1</h1>
+      <div className={classes.counter__wrapper}>
+        <h1>1</h1>
 
-      <div>
-        <button>Decrement</button>
-        <button>Increment</button>
-      </div>
+        <div className={classes.counter__operations}>
+          <div className={classes.counter__buttons}>
+            <button>Decrement</button>
+            <button>Increment</button>
+          </div>
 
-      <div>
-        <input type="number" />
-        <button>Increment by Amount</button>
+          <div className={classes.counter__amount}>
+            <input type="number" />
+            <button>Increment by Amount</button>
+          </div>
+        </div>
       </div>
     </div>
   );
